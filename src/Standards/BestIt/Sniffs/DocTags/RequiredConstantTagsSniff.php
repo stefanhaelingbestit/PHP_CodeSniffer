e1a6cf639bf4a7f803d901fa0aa2bbc8004a1ec3
Stefan Häling <stefan.haeling@bestit-online.de>
2018-10-23 08:29:47 +0200

    Removed function that requires the var tag in annotations.

--- src/Standards/BestIt/Sniffs/DocTags/RequiredConstantTagsSniff.php
+++ src/Standards/BestIt/Sniffs/DocTags/RequiredConstantTagsSniff.php
@@ -15,21 +15,4 @@ use BestIt\Sniffs\ConstantRegistrationTrait;
 class RequiredConstantTagsSniff extends AbstractRequiredTagsSniff
 {
     use ConstantRegistrationTrait;
-
-    /**
-     * Returns the required tag data.
-     *
-     * The order in which they appear in this array os the order for tags needed.
-     *
-     * @return array List of tag metadata
-     */
-    protected function getTagRules(): array
-    {
-        return [
-            'var' => [
-                'min' => 1,
-                'max' => 1
-            ],
-        ];
-    }
 }
